81867406b311b0bf5800e6fd33753a6190c43f85
Magnus-90 <magnus.hansen@optimatik.ch>
2026-04-23 11:50:59 +0200

    Add CheckoutComponent and finished Styles with validation for the inputs

--- src/app/checkout-component/checkout-component.ts
+++ src/app/checkout-component/checkout-component.ts
@@ -3,6 +3,7 @@ import { NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CheckoutService } from '../checkout-service';
 import { Router, RouterLink } from '@angular/router';
+import { CartService } from '../cart-service';
 
 @Component({
   selector: 'app-checkout-component',
@@ -11,17 +12,20 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './checkout-component.scss',
 })
 export class CheckoutComponent {
+  public cartService = inject(CartService);
   firstName = '';
   lastName = '';
   email = '';
   address = '';
+  paymentmethod = '';
   private checkoutService = inject(CheckoutService);
   private router = inject(Router);
   confirmOrder() {
   const success = this.checkoutService.confirmOrder({
     name: this.firstName + ' ' + this.lastName,
     email: this.email,
-    address: this.address
+    address: this.address,
+    paymentmethod: this.paymentmethod
   });
 
   if (success) {
--- src/app/checkout-service.ts
+++ src/app/checkout-service.ts
@@ -8,17 +8,25 @@ import { NotificationService } from './notification-service';
 export class CheckoutService {
   private cartService = inject(CartService);
   private notificationService = inject(NotificationService);
-  confirmOrder(costumerData: {name: string; email: string; address: string;}){
+  confirmOrder(costumerData: {name: string; email: string; address: string; paymentmethod: string;}){
     if (this.cartService.cartItems().length === 0){
       this.notificationService.show("Dein Warenkorb ist leer");
       return false;
     }
     if (costumerData.name.length < 3){
       this.notificationService.show("Bitte gib einen gültigen Namen ein.");
-      return;
+      return false;
     }
     if (!costumerData.email.includes('@')){
-      this.notificationService.show("Deine E-Mail Adresse ist ungültig");
+      this.notificationService.show("Deine E-Mail Adresse ist ungültig.");
+      return false;
+    }
+    if (costumerData.address.length < 5){
+      this.notificationService.show("Bitte gib eine gültige Adresse ein.");
+      return false;
+    }
+    if (!costumerData.paymentmethod){
+      this.notificationService.show("Bitte wähle eine Zahlungsmethode.");
       return false;
     }
     this.cartService.clearCart();
